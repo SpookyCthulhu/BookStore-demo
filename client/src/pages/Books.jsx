77598b87d80532b86415585af13429e95a2c311f
Michelle <161183828+SpookyCthulhu@users.noreply.github.com>
2024-06-06 16:38:07 -0400

    Fixed graphical errors
    build is stable and ready to merge with main

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,59 +1,63 @@
-import { Link } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Books = () => {
+  const [books, setBooks] = useState([]);
 
-	const [books, setBooks] = useState([])
-	
-	useEffect(()=>{
-		const fetchAllBooks = async ()=>{
-			try{
-				const res = await axios.get('http://localhost:8800/books')
-				setBooks(res.data)
-			}catch(err){
-				console.log(err)
-			}
-		}
-		fetchAllBooks()
-	}, [])
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get('http://localhost:8800/books');
+        setBooks(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, []);
 
-	const handleDelete = async (id)=>{
-		try{
-			await axios.delete('http://localhost:8800/books/'+id)
-			window.location.reload()
-		} catch(err){
-			console.log(err)
-		}
-	}
-	return (
-		<div class='category'>
-			<h1>Adventure</h1>
-			<div className='books'>
-				{books.map((book) => (
-					<div className='book' key={book.id}>
-						{
-						//enter your images in public/images for them to render!
-						}
-						{book.cover && <img src={`images/${book.cover}`} alt='x' />}
-						<h2>{book.title}</h2>
-						<p>{book.description}</p>
-						<button>Add to cart - ${book.price}</button>
-						{
-						//<div class='buttons'>
-						//	<button className='delete' onClick={()=>handleDelete(book.id)}>Delete</button>
-						//	<button className='update'><Link class='link' to={`/update/${book.id}`}>update</Link></button>
-						//</div>
-						}
-					</div>
-				))}	
-			<button class='addBtn'>
-			<Link class='link white' to="/add">+</Link>
-			</button>
-			</div>
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete('http://localhost:8800/books/' + id);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-		</div>
-	);
+  return (
+    <div className="category">
+      <h1>Adventure</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div className="book" key={book.id}>
+            <div className="imgContainer">
+              {book.cover && <img src={`images/${book.cover}`} alt="x" />}
+              <div className="overlay">
+                <button className="overlay-button" onClick={() => handleDelete(book.id)}>
+                  <i className="fas fa-trash-alt"></i>
+                </button>
+                <Link to={`/update/${book.id}`} className="overlay-button">
+                  <i className="fas fa-pencil-alt"></i>
+                </Link>
+              </div>
+            </div>
+            <h2 class='title'>{book.title}</h2>
+            <p class='description'>{book.description}</p>
+            <button className="add-to-cart">
+              <i className="fas fa-shopping-cart"></i> Add to cart - ${book.price}
+            </button>
+          </div>
+        ))}
+        <button className="addBtn">
+          <Link className="link white" to="/add">
+            +
+          </Link>
+        </button>
+      </div>
+    </div>
+  );
 };
 
-export default Books
+export default Books;
